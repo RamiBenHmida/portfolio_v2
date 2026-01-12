b3e1494f6f2cfcdf3d437d613feff1ba58e83d1a
RamiBenHmida <benhmida.yami@gmail.com>
2026-01-12 16:18:14 +0100

    removed versel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-// Import Vercel Speed Insights
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import React from "react";
-
-createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <>
-      {/* Add Speed Insights at top level */}
-      <SpeedInsights />
-
-      <App />
-    </>
-  </React.StrictMode>
-);
+createRoot(document.getElementById("root")!).render(<App />);
